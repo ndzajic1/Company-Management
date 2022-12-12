0f15a4d47802c0bfb0ffc4d33b1484d802dacae7
ndzajic1 <ndzajic1@etf.unsa.ba>
2022-12-12 12:51:54 +0100

    db test

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,20 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        Connection con= DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7584126",
+         "sql7584126","");
+        Statement st=con.createStatement();
+
+       ResultSet rs= st.executeQuery(
+                "select * from Employees"
+        );
+       while(rs.next()){
+           System.out.print(rs.getInt(1)+" "+
+           rs.getString(2)+" \n");
+       }
     }
 }
