21f2daa27ea7a763689994fe3e1ce4fc2746d4dd
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-01-01 00:07:43 +0100

    new jobs class design

--- src/main/java/ba/unsa/etf/rpr/Job.java
+++ src/main/java/ba/unsa/etf/rpr/Job.java
@@ -1,4 +1,71 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
 public class Job {
+    int id;
+    String jobTitle;
+    double minSalary, maxSalary;
+
+    public Job(int id, String jobTitle, double minSalary, double maxSalary) {
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public double getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(double maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id && Double.compare(job.minSalary, minSalary) == 0 && Double.compare(job.maxSalary, maxSalary) == 0 && jobTitle.equals(job.jobTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, jobTitle, minSalary, maxSalary);
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "id=" + id +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", minSalary=" + minSalary +
+                ", maxSalary=" + maxSalary +
+                '}';
+    }
 }
