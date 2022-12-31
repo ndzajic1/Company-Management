7e9bbae1617c2e4d6dc7177c317a22dc2358a706
ndzajic1 <ndzajic1@etf.unsa.ba>
2022-12-31 23:35:06 +0100

    new departments class design

--- src/main/java/ba/unsa/etf/rpr/Department.java
+++ src/main/java/ba/unsa/etf/rpr/Department.java
@@ -1,4 +1,72 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
 public class Department {
+    int id;
+    String name;
+    String location;
+    Employee manager;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", manager=" + manager +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && name.equals(that.name) && Objects.equals(location, that.location) && Objects.equals(manager, that.manager);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, location, manager);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Department(int id, String name, String location, Employee manager) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.manager = manager;
+    }
 }
