ac6783533dc928f010f73b3ea28380d6403580b8
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-01-01 16:12:21 +0100

    jobs rows inserted

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -5,16 +5,21 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) throws SQLException {
 
-        Connection con= DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7584126",
-         "sql7584126","w5YWmSvFRu");
+        Connection con= DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_rpr_db",
+         "freedb_ndzajic1","");
         Statement st=con.createStatement();
-
-       ResultSet rs= st.executeQuery(
-                "select * from Employees"
+        st.executeUpdate(
+                "INSERT INTO `freedb_rpr_db`.`Jobs` (`id`, `job_title`, `min_salary`, `max_salary`) VALUES ('7', 'Public Accountant', '4200', '9000');"
         );
+       ResultSet rs= st.executeQuery(
+                "select * from Jobs"
+        );
+
        while(rs.next()){
            System.out.print(rs.getInt(1)+" "+
            rs.getString(2)+" \n");
        }
+
+
     }
 }
