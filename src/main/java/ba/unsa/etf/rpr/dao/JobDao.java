4783974bdcddd228323e3956f4e908dcc6330619
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-01-01 23:34:51 +0100

    department&job dao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/JobDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.Job;
+
+import java.util.List;
+
+public interface JobDao extends Dao<Job>{
+    List<Job> getByTitle(String txt);
+    List<Job> getBySalaryRange(double floor, double ceiling);
+
+}
