1e9e5515428ff2ad7cdc75b34f9912502e2279b9
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-02-09 10:59:53 +0100

    javafx

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,12 +1,24 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
-public class App {
-    public static void main(String[] args) throws SQLException, IOException {
-        Properties p = new Properties();
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class App extends Application {
+
+
+   public static void main(String[] args) throws SQLException, IOException {
+        launch(args);
+
+       /* Properties p = new Properties();
         p.load(ClassLoader.getSystemResource("application.properties.sample").openStream());
         String url = p.getProperty("db.connection_string");
         String username = p.getProperty("db.username");
@@ -21,5 +33,16 @@ public class App {
            System.out.print(rs.getInt(1)+" "+
            rs.getString(2)+" "+ rs.getString(3)+" \n");
        }
+
+        */
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent p = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
+        stage.setTitle("Company");
+        stage.setScene(new Scene(p, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
