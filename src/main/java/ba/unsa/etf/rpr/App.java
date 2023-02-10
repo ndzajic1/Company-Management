e0071aecaf315a86c46a39d46d6d8001828c00c4
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-02-10 22:20:13 +0100

    RemoveEmployee.fxml

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -39,7 +39,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent p = FXMLLoader.load(getClass().getResource("/fxml/EditEmployee.fxml"));
+        Parent p = FXMLLoader.load(getClass().getResource("/fxml/RemoveEmployee.fxml"));
         stage.setTitle("Company");
         stage.setScene(new Scene(p, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         //stage.getIcons()
