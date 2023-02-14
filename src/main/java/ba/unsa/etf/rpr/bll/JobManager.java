5dee5d2c621ca512b1f0c4035a81814d9f81aa6c
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-02-14 16:04:41 +0100

    job manager

--- src/main/java/ba/unsa/etf/rpr/bll/JobManager.java
+++ src/main/java/ba/unsa/etf/rpr/bll/JobManager.java
@@ -14,21 +14,37 @@ public class JobManager implements Manageable<Job>{
 
     @Override
     public void add(Job j) {
+        try{
+            validateJobTitle(j);
+            DaoFactory.jobDao().add(j);
+        } catch(SQLException e){
+            // my except
+        }
+    }
 
+    private void validateJobTitle(Job j) throws SQLException {
+        for(Job job : getAll()){
+            if(job.getTitle().equals(j.getTitle()))
+                throw new RuntimeException("duplicate");
+        }
     }
 
     @Override
     public void update(Job j) {
-
+        DaoFactory.jobDao().update(j);
     }
 
     @Override
     public void delete(int id) {
-
+        try {
+            DaoFactory.jobDao().delete(id);
+        } catch (RuntimeException e){
+            // foreign key constraint
+        }
     }
 
     @Override
-    public List<Job> getAll() {
-        return null;
+    public List<Job> getAll() throws SQLException {
+        return DaoFactory.jobDao().getAll();
     }
 }
