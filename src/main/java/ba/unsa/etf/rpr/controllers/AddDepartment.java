4c0353291888e577d02814d3fe4ea6183a29953e
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-02-14 17:02:01 +0100

    add dept controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddDepartment.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class AddDepartment {
+    public Button addButton;
+    public TextField deptName;
+    public TextField location;
+    public ChoiceBox managers;
+
+    public void addDept(ActionEvent actionEvent) {
+    }
+}
