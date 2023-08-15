85669a9376015859afbf30f068813b2f0b524a77
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-15 22:37:02 +0200

    del employee

--- src/main/java/ba/unsa/etf/rpr/controllers/RemoveEmployee.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RemoveEmployee.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.bll.EmployeeManager;
+import ba.unsa.etf.rpr.domain.Employee;
+import javafx.event.ActionEvent;
+
 public class RemoveEmployee {
+
+    private Employee employee;
+    private EmployeeManager employeeManager = new EmployeeManager();
+
+    public RemoveEmployee(Employee e){
+        this.employee = e;
+    }
+
+    public void removeEmployee(ActionEvent actionEvent) {
+        employeeManager.deleteEmployee(employee.getId());
+    }
 }
