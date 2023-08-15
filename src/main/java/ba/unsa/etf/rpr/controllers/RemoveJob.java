9eb0f42eb377d3d2c584b30e0979a9314a069f3d
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-15 22:43:17 +0200

    del job

--- src/main/java/ba/unsa/etf/rpr/controllers/RemoveJob.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RemoveJob.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.bll.DepartmentManager;
+import ba.unsa.etf.rpr.bll.JobManager;
+import ba.unsa.etf.rpr.domain.Department;
+import ba.unsa.etf.rpr.domain.Job;
+import javafx.event.ActionEvent;
+
 public class RemoveJob {
+    private Job job;
+    private JobManager jobManager = new JobManager();
+
+    public RemoveJob(Job j){
+        this.job = j;
+    }
+
+    public void removeJob(ActionEvent actionEvent) {
+        // moze baciti exc
+        jobManager.deleteJob(job.getId());
+    }
 }
