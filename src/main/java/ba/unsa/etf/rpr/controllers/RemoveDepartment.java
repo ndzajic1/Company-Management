e28d20d781509f06622eb97816803cc4e8ed0544
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-15 22:40:55 +0200

    del dept

--- src/main/java/ba/unsa/etf/rpr/controllers/RemoveDepartment.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RemoveDepartment.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.bll.DepartmentManager;
+import ba.unsa.etf.rpr.bll.EmployeeManager;
+import ba.unsa.etf.rpr.domain.Department;
+import ba.unsa.etf.rpr.domain.Employee;
+import javafx.event.ActionEvent;
+
 public class RemoveDepartment {
+    private Department department;
+    private DepartmentManager departmentManager = new DepartmentManager();
+
+    public RemoveDepartment(Department d){
+        this.department = d;
+    }
+
+    public void removeDept(ActionEvent actionEvent) {
+        // moze baciti exc
+        departmentManager.deleteDept(department.getId());
+    }
 }
