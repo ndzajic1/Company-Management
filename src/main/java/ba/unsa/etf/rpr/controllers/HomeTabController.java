7ca528910faa2526ed8221ff5247da4c01beb782
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-16 20:52:04 +0200

    home tab

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeTabController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HomeTabController {
+
+    @FXML
+    private Button changePasswordButton;
+
+    @FXML
+    private Label deptManager;
+
+    @FXML
+    private Label deptName;
+
+    @FXML
+    private Label employeeName;
+
+    @FXML
+    void changePassword(ActionEvent event) {
+
+    }
+
+}
