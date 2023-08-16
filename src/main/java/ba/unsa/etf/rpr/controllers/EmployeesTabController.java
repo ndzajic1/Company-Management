37b4cdafdf94371b1d9b142282603cba3ca45e3d
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-16 20:54:47 +0200

    employees tab

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeesTabController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class EmployeesTabController {
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    void searchEmployees(ActionEvent event) {
+
+    }
+
+}
