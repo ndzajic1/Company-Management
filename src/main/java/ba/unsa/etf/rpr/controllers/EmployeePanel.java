1fc1fbfc6e5820ff2d9aac5c60acca455df5eb6c
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-16 17:45:51 +0200

    new fxmls

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel.java
@@ -5,16 +5,26 @@ import ba.unsa.etf.rpr.domain.LoggableUser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 
 public class EmployeePanel {
 
     private Employee user;
+
+    //tabs
+    public Tab homeTab;
+    public Tab employeesTab;
+    public Tab departmentsTab;
+    public Tab jobsTab;
+
+
     //home tab controls
     public Label employeeName;
     public Label deptName;
     public Label deptManager;
 
     public EmployeePanel(Employee user){
+
         this.user = user;
     }
 
@@ -33,6 +43,9 @@ public class EmployeePanel {
     @FXML
     void  initialize(){
         homeTabInitialize();
+        // roles simul
+        if(!user.isAdmin() || !user.getDepartment().getManager().equals(user))
+            employeesTab.setDisable(true);
     }
 
     public void changePassword(ActionEvent actionEvent) {
