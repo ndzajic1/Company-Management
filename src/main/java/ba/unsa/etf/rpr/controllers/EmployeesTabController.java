4bf8a9a5c3d109a33b71034ccb34f65a718d6dab
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-16 21:13:28 +0200

    employees new tab

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeesTabController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeesTabController.java
@@ -3,11 +3,62 @@ package ba.unsa.etf.rpr.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class EmployeesTabController {
 
     @FXML
+    private Button addButton;
+
+    @FXML
+    private TableColumn<?, ?> dateHiredCol;
+
+    @FXML
+    private TableColumn<?, ?> deptNameCol;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private TableView<?> employeesTable;
+
+    @FXML
+    private TableColumn<?, ?> firstNameCol;
+
+    @FXML
+    private TableColumn<?, ?> jobTitleCol;
+
+    @FXML
+    private TableColumn<?, ?> lastNameCol;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private TableColumn<?, ?> salaryCol;
+
+    @FXML
     private Button searchButton;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    void addEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeEmployee(ActionEvent event) {
+
+    }
 
     @FXML
     void searchEmployees(ActionEvent event) {
