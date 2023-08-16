255518d3b266e13c818abf2f204d6f2c6a4a41b6
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-16 23:12:50 +0200

    job tab controller

--- src/main/java/ba/unsa/etf/rpr/controllers/JobsTabController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/JobsTabController.java
@@ -1,34 +1,82 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.bll.EmployeeManager;
+import ba.unsa.etf.rpr.bll.JobManager;
+import ba.unsa.etf.rpr.domain.Department;
+import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.domain.Job;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class JobsTabController {
+    
+    private JobManager jobManager = new JobManager();
+    private EmployeeManager employeeManager = new EmployeeManager();
+    
+    private List<Job> jobsList;
+
+    @FXML
+    private TableView<Job> jobsTable;
+
+    @FXML
+    private TableColumn<Job, String> titleCol;
+
+    @FXML
+    private TableColumn<Job, String> minSalaryCol;
+    
+    @FXML
+    private TableColumn<Job, String> maxSalaryCol;
+
+    @FXML
+    private TableColumn<Job, String> numOfEmployeesCol;
 
     @FXML
     private Button addButton;
-
     @FXML
     private Button editButton;
-
-    @FXML
-    private TableView<?> jobsTable;
-
-    @FXML
-    private TableColumn<?, ?> maxSalaryCol;
-
-    @FXML
-    private TableColumn<?, ?> minSalaryCol;
-
-    @FXML
-    private TableColumn<?, ?> numOfEmployeesCol;
-
     @FXML
     private Button removeButton;
 
-    @FXML
-    private TableColumn<?, ?> titleCol;
 
+    
+    @FXML
+    void initialize() throws SQLException {
+        jobsList = jobManager.getAllJobs();
+        Map<Job, Integer> employeesPerJob = new TreeMap<>();
+        for(Job j : jobsList){
+            employeesPerJob.put(j,0);
+        }
+        
+        for(Employee e : employeeManager.getAllEmployees()){
+            int curr = employeesPerJob.get(e.getJob());
+            employeesPerJob.put(e.getJob(), curr + 1);
+        }
+        
+        titleCol.setCellValueFactory(new JobCellValueFactory("Job Title"));
+        minSalaryCol.setCellValueFactory(new JobCellValueFactory("Minimal Salary"));
+        maxSalaryCol.setCellValueFactory(new JobCellValueFactory("Maximal Salary"));
+        numOfEmployeesCol.setCellValueFactory(new JobCellValueFactory(employeesPerJob));
+
+
+
+        jobsTable.setItems(FXCollections.observableArrayList(jobsList));
+    }
+
+    public void addJob(ActionEvent actionEvent) {
+    }
+
+    public void editJob(ActionEvent actionEvent) {
+    }
+
+    public void removeJob(ActionEvent actionEvent) {
+    }
 }
