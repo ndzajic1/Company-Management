81c704e70b4e37821b3b560e9a706f214912fd5f
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-27 03:10:16 +0200

    edit job controller

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel/JobsTab/EditJobController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel/JobsTab/EditJobController.java
@@ -4,13 +4,17 @@ import ba.unsa.etf.rpr.bll.JobManager;
 import ba.unsa.etf.rpr.domain.Job;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
 public class EditJobController {
     private JobManager jobManager = new JobManager();
+    private JobsTabController mainController;
     private Job job;
 
 
@@ -26,26 +30,35 @@ public class EditJobController {
     private SimpleStringProperty maxSalaryProperty;
 
 
-    public EditJobController(Job j) throws SQLException {
+    public EditJobController(Job j, Object o) throws SQLException {
         this.job = j;
+        mainController = (JobsTabController) o;
         titleProperty = new SimpleStringProperty(j.getTitle());
         minSalaryProperty = new SimpleStringProperty(Double.toString(j.getMinSalary()));
         maxSalaryProperty = new SimpleStringProperty(Double.toString(j.getMaxSalary()));
 
     }
 
+    @FXML
     public void initialize(){
         title.textProperty().bindBidirectional(titleProperty);
         minSalary.textProperty().bindBidirectional(minSalaryProperty);
         maxSalary.textProperty().bindBidirectional(maxSalaryProperty);
     }
 
-    public void editJob(ActionEvent actionEvent) {
+    @FXML
+    public void editJob(ActionEvent actionEvent) throws SQLException {
 
         job.setTitle(titleProperty.getValue());
         job.setMinSalary(Double.parseDouble(minSalaryProperty.getValue()));
         job.setMaxSalary(Double.parseDouble(maxSalaryProperty.getValue()));
 
         jobManager.updateJob(job);
+
+        mainController.refreshTable();
+
+        Node n = (Node) actionEvent.getSource();
+        Stage currStage = (Stage) n.getScene().getWindow();
+        currStage.close();
     }
 }
