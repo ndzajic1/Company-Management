811c41f242a08f204e79c443484961eec8b58e4c
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-27 03:12:08 +0200

    rm job controller

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel/JobsTab/RemoveJobController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeePanel/JobsTab/RemoveJobController.java
@@ -3,17 +3,29 @@ package ba.unsa.etf.rpr.controllers.EmployeePanel.JobsTab;
 import ba.unsa.etf.rpr.bll.JobManager;
 import ba.unsa.etf.rpr.domain.Job;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class RemoveJobController {
     private Job job;
     private JobManager jobManager = new JobManager();
 
-    public RemoveJobController(Job j){
+    private JobsTabController mainController;
+
+    public RemoveJobController(Job j, Object o){
         this.job = j;
+        this.mainController = (JobsTabController) o;
     }
 
-    public void removeJob(ActionEvent actionEvent) {
+    public void removeJob(ActionEvent actionEvent) throws SQLException {
         // moze baciti exc
         jobManager.deleteJob(job.getId());
+        mainController.refreshTable();
+
+        Node n = (Node) actionEvent.getSource();
+        Stage currStage = (Stage) n.getScene().getWindow();
+        currStage.close();
     }
 }
