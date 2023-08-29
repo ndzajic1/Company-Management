ddb4e7b66b35a43a8902d9a5c81c7b303b9cd5a2
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-30 01:30:58 +0200

    JobManager exception handling

--- src/main/java/ba/unsa/etf/rpr/bll/JobManager.java
+++ src/main/java/ba/unsa/etf/rpr/bll/JobManager.java
@@ -2,50 +2,53 @@ package ba.unsa.etf.rpr.bll;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Job;
+import ba.unsa.etf.rpr.exceptions.CompanyException;
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class JobManager{
 
-    public Job getJobById(int id) throws SQLException {
+    public Job getJobById(int id) throws CompanyException {
         return DaoFactory.jobDao().getById(id);
     }
 
 
-    public void addNewJob(Job j) {
-        try{
-            validateJobTitle(j);
-            DaoFactory.jobDao().add(j);
-        } catch(SQLException e){
-            // already exists
-        }
+    public void addNewJob(Job j) throws SQLException, CompanyException {
+        validateJob(j);
+        DaoFactory.jobDao().add(j);
     }
 
-    private void validateJobTitle(Job j) throws SQLException {
+    private void validateJob(Job j) throws SQLException, CompanyException {
+        if(j.getMinSalary() > j.getMaxSalary())
+            throw new CompanyException("Check salaries again.");
         for(Job job : getAllJobs()){
             if(job.getTitle().equals(j.getTitle()))
-                throw new RuntimeException("duplicate");
+                throw new CompanyException("Job with the same name already exists.");
         }
     }
 
 
-    public void updateJob(Job j) {
+    public void updateJob(Job j) throws CompanyException {
 
         DaoFactory.jobDao().update(j);
     }
 
 
-    public void deleteJob(int id) {
+    public void deleteJob(int id) throws CompanyException {
         try {
             DaoFactory.jobDao().delete(id);
-        } catch (RuntimeException e){
-            // foreign key constraint
+        } catch (CompanyException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new CompanyException("There are employees on this job! Change their jobs first.");
+            }
+            throw e;
         }
     }
 
 
-    public List<Job> getAllJobs() throws SQLException {
+    public List<Job> getAllJobs() throws CompanyException {
         return DaoFactory.jobDao().getAll();
     }
 }
