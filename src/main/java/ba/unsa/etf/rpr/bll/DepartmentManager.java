7c89f3b2523208c14938e88b76059aa5ad75f418
ndzajic1 <ndzajic1@etf.unsa.ba>
2023-08-30 01:26:58 +0200

    DepartmentManager exception handling

--- src/main/java/ba/unsa/etf/rpr/bll/DepartmentManager.java
+++ src/main/java/ba/unsa/etf/rpr/bll/DepartmentManager.java
@@ -4,53 +4,57 @@ import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Department;
 import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.exceptions.CompanyException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class DepartmentManager {
 
-    public Department getDeptById(int id) throws SQLException {
+    public Department getDeptById(int id) throws CompanyException {
         return DaoFactory.departmentDao().getById(id);
     }
 
 
-    public void addNewDept(Department d) {
+    public void addNewDept(Department d) throws CompanyException {
         try{
             validateDeptName(d);
             DaoFactory.departmentDao().add(d);
             Employee manager = d.getManager();
             manager.setDepartment(d);
             DaoFactory.employeeDao().update(manager);
-        } catch(SQLException e){
-            // my except
+        } catch(CompanyException | SQLException e){
+            throw new CompanyException(e.getMessage(), e);
         }
     }
 
-    private void validateDeptName(Department d) throws SQLException {
+    private void validateDeptName(Department d) throws CompanyException, SQLException {
         for(Department dept : getAllDepts()){
             if(dept.getName().equals(d.getName()))
-                throw new RuntimeException("duplicate");
+                throw new CompanyException("Department with the same name already exists.");
         }
     }
 
 
-    public void updateDept(Department d) {
+    public void updateDept(Department d) throws CompanyException {
 
         DaoFactory.departmentDao().update(d);
     }
 
 
-    public void deleteDept(int id) {
+    public void deleteDept(int id) throws CompanyException {
         try {
             DaoFactory.departmentDao().delete(id);
-        } catch (RuntimeException e){
-            // foreign key constraint
+        } catch  (CompanyException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new CompanyException("Move employees from this department to another one first!");
+            }
+            throw e;
         }
     }
 
 
-    public List<Department> getAllDepts() throws SQLException {
+    public List<Department> getAllDepts() throws CompanyException {
         return DaoFactory.departmentDao().getAll();
     }
 }
